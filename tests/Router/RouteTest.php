b0a03dc70a57eb1ccd39797c14229396947c9b1e
Wolfy-J <wolfy.jd@gmail.com>
2018-09-13 16:17:56 +0300

    more tests

---
+++ tests/Router/RouteTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Router\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Spiral\Router\Route;
+use Zend\Diactoros\ServerRequest;
+
+class RouteTest extends TestCase
+{
+    public function testPrefix()
+    {
+        $route = new Route("/action", Call::class);
+        $this->assertSame("", $route->getPrefix());
+        $this->assertSame("/something", $route->withPrefix("/something")->getPrefix());
+        $this->assertSame("", $route->getPrefix());
+    }
+
+    /**
+     * @expectedException \Spiral\Router\Exceptions\RouteException
+     */
+    public function testContainerException()
+    {
+        $route = new Route("/action", Call::class);
+        $route->handle(new ServerRequest());
+    }
+}
